4acd39b465a60cd4629e7e690eb056b2ea1b8c28
malmossa <malmossa@gmail.com>
2021-03-11 18:18:53 -0800

    Completed

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link'
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
@@ -9,6 +10,7 @@ export default function Home() {
       <Navbar />
       <h1>Home Page</h1>
       <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+      <Link href="/bloggers"><a>See all Bloggers</a></Link>
       <Footer />
     </div>
   )
